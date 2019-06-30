90f1e2559e1e4fe52bed3301ae2a4d50c69bb353
Mariusz Klec <mklec@pjwstk.edu.pl>
2019-06-30 03:27:36 +0200

    przed deployem wraz z katalogiem build

---
+++ build/precache-manifest.f421215bbc6c669b682cd5a9f502b027.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19281eb0bc7c460bb055b960bdf7ee03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8be7717980c89491a7f2",
+    "url": "/static/css/main.b02e6d5f.chunk.css"
+  },
+  {
+    "revision": "27ab39d71c72d61e627f",
+    "url": "/static/js/2.a2954486.chunk.js"
+  },
+  {
+    "revision": "8be7717980c89491a7f2",
+    "url": "/static/js/main.89af2eaa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b32f9ad299b8289ff50c6254bc4c372e",
+    "url": "/static/media/homeDecoBg.b32f9ad2.png"
+  },
+  {
+    "revision": "0ea93d1c58804772e174ffaf09e0ff1f",
+    "url": "/static/media/logo-clarin-pl-short.0ea93d1c.png"
+  },
+  {
+    "revision": "ab822e22fbde3c2c925411a6cd112942",
+    "url": "/static/media/logo-clarin-pl.ab822e22.png"
+  }
+]);
