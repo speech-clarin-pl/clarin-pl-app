b6e6f99737c3f20bc65efc1e4129e0134c04425d
test <mklec@pjwstk.edu.pl>
2020-06-28 19:48:19 +0200

    gotowy do 1 deploymentu

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.12e921024cce3b8520b3fd95c1bea5ac.js"
+  "/precache-manifest.b6eb79ac0731d59a2b965febf09c6603.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
